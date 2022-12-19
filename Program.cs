1dca967d0309ad7b761d19ae396250f094cccf95
ihsan <42092819+ihsanfil@users.noreply.github.com>
2022-12-19 13:59:09 +0300

    metotlar

--- Program.cs
+++ Program.cs
@@ -1,352 +1,52 @@
 ﻿using System;
 
-namespace degisken 
+namespace degisken
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-        //     //Console.WriteLine("Hello World!");
-        //     int deger = 2;
-        //     string degisken = " ";
-        //     byte b = 5;
-        //     short s = 5;
-        //     ushort us= 5;
-        //     int i= 2;
-        //     Int16 i16= 1;
-        //     Int32 i32 = 32;
-        //     Int64 i64 =64;
-
-        //     uint ui = 2;
-        //     long l =2;
-        //     ulong ul = 2;
-            
-        //     float f=2;
-        //     double d= 2;
-        //     decimal de= 2;
-
-        //     char c='c';
-        //     string c2= "strng";
-
-        //     bool tr =true;  
-
-        //     DateTime dt= DateTime.Now;
-        //     //Console.WriteLine(dt);
-        //     object o1 = "x";
-        //     object o2 = 'c';
-        //     string str1  =string.Empty;
-        //     str1 = "Test";
-        //     string ad = "Zikriye";
-        //     string soyad = "Ürkmez";
-        //     string adSoyad = ad+" "+soyad;
-        //     //Console.WriteLine(adSoyad);
-            
-        //     int int1=5;
-        //     int int2=3;
-        //     int  int3 = int1* int2;
-        //     //Console.WriteLine(int3);
-
-        //     bool b1 = 10<2;
-        //     //Console.WriteLine(b1);
-
-        //    string st22 = "20";
-        //    int int20 = 20;
-
-        //    string strdeger = st22 + int20.ToString();
-        //   // Console.WriteLine(strdeger);
-
-        //    int toplam = Convert.ToInt32(st22) + int20;
-        //    int toplam1 = int20 + int.Parse(st22);
-        //   // Console.WriteLine(toplam1);
-        //   string gun = DateTime.Now.ToString("dd-MM-yyyy");
-        //   string gun1 = DateTime.Now.ToString("M'/'dd'/'yy");
-        //   string gun2 = DateTime.Now.ToString("dd.MM.yyyy");
-        //   string saat = DateTime.Now.ToString("HH:mm");
-        //   Console.WriteLine(gun);
-        //   Console.WriteLine(gun1);
-        //   Console.WriteLine(gun2);
-        //   Console.WriteLine(saat);
-
-        /////
-
-            // int x =53;
-            // int y =54;
-            // y = y+2;
-            // Console.WriteLine(y);
-            // y +=2;
-            // Console.WriteLine(y);
-            // y/=1;
-            // Console.WriteLine(y);
-            // x *=2;
-            // Console.WriteLine(x);
-
-
-            // ///////////
-
-            // bool isSucces = true;
-            // bool isCompleted = false;
-
-            // if(isSucces && isCompleted)
-            //     Console.WriteLine("Mükemmel!");
-
-            // if(isSucces || isCompleted)
-            //     Console.WriteLine("Harika!");
-            
-            // if (isSucces && isCompleted)
-            //     Console.WriteLine("İyi!");
-
-        
-            // /////////////////
-
-            // int a = 3;
-            // int b = 4;
-            // bool sonuc = a<b; 
-
-            // Console.WriteLine(sonuc);
-            // sonuc = a>b;
-            // Console.WriteLine(sonuc);
-            // sonuc = a>=b;
-            // Console.WriteLine(sonuc);
-            // sonuc = a<=b;
-            // Console.WriteLine(sonuc);
-            // sonuc = a==b;
-            // Console.WriteLine(sonuc);
-            // sonuc = a!=b;
-            // Console.WriteLine(sonuc);
-
-            // /////////////
-
-            // int sayi1 = 10;
-            // int sayi2 = 5;
-            // int sonuc1 = sayi1/sayi2;
-            // Console.WriteLine(sonuc1);
-            // sonuc1 = sayi1+sayi2;
-            // Console.WriteLine(sonuc1);
-            // sonuc1 = sayi1+sayi2;
-            // Console.WriteLine(sayi1);
-            // sonuc1 = sayi1++;
-
-            // int sonuc2 = 20%3;
-            // Console.WriteLine(sonuc2); 
-        //    Console.WriteLine("******* Implicit Conversion / Bilinçsiz dönüşüm ************"); 
-        //    byte a = 3;
-        //    sbyte b= 7;
-        //    short c = 10;
-
-        //    int d = a+b+c;
-        //    Console.WriteLine("d: "+d);
-
-        //    long h = d;
-        //    Console.WriteLine("h: "+h);
-
-
-        //     float i = h;
-        //    Console.WriteLine("i: "+i);
-
-        //    string z = "zikriye";
-        //    char f = 'ü';
-        //    object o = z+" "+f+". "+i;
-        //    Console.WriteLine(o);
+                                     
+          static int Topla (int a, int b){
+            return a+b;
+          }
           
-        //    Console.WriteLine("******* Explicit Conversion / Bilinçli dönüşüm ************"); 
-        //    int x =4;
-        //    byte y = (byte)x;
- 
-        //  Console.WriteLine("y: " + y);
-        //      int br = 100;
-        //      byte t = (byte)br;
-        //  Console.WriteLine("t: " + t);
+          int sonuc = Topla(10,43);
+          // Console.WriteLine(sonuc);
 
-        //      float w = 10.3f;
-        //      byte v = (byte)w;
-        //  Console.WriteLine("v: " + v);
-        //   /**** To String*/
-        //  Console.WriteLine("*** To String ***"); 
-        // int xx = 53;
-        // string yy = xx.ToString();
-        //  Console.WriteLine(yy); 
-        // /**** Convert*/
-        // string xx1 = "53";
-        // string xx2 = "1";
+          Metotlar metot = new Metotlar();
+          // metot.EkranaYazdir(Convert.ToString(sonuc));
 
-        // int toplam = Convert.ToInt32(xx1)+ Convert.ToInt32(xx2);
-        // Console.WriteLine("toplam: "+toplam); 
+          // int carp= metot.Carp(10,3);
+          // metot.EkranaYazdir(Convert.ToString(carp));
+          int a= 10;
+          int b= 43;
+          int sonuc1 = Topla(a,b);
+          int sonuc2 = metot.ArttirveTopla(ref a,ref b);
+          metot.EkranaYazdir(Convert.ToString(sonuc1));
+          metot.EkranaYazdir(Convert.ToString(sonuc2));
+          metot.EkranaYazdir(Convert.ToString(a));
+          metot.EkranaYazdir(Convert.ToString(b));
 
-        // try
-        // {
-        //     int a = int.Parse(Console.ReadLine());
-
-        //     int b = int.Parse(Console.ReadLine());
-
-        //     int c = a+b;
-
-        //     Console.WriteLine(c);
-        // }
-        // catch(Exception ex)
-        // {
-        //     Console.WriteLine("Bir Hata Oluştu: "+ ex.Message);
-        // }
-        // finally
-        // {
-        //     Console.WriteLine("İşlem tamamlandı.");
-        // }
-        
-        // }
-        //  int time = DateTime.Now.Hour;
-        //  if (time <= 18)
-        //  {
-        //     Console.WriteLine("İyi Günler");
-        //  }else{
-        //       Console.WriteLine("İyi Akşamlar");
-        //  }  
-        // int month = DateTime.Now.Month;
-            // switch (month)
-            // {
-            //     case 1:
-            //     Console.WriteLine("ocak ayındasınız");
-            //     break;
-            //     case 2:
-            //     Console.WriteLine("şubat ayındasınız");
-            //     break;
-            //     case 3:
-            //     Console.WriteLine("mart ayındasınız");
-            //     break;
-            //     case 4:
-            //     Console.WriteLine("nisan ayındasınız");
-            //     break;
-            //     case 5:
-            //     Console.WriteLine("mayıs ayındasınız");
-            //     break;
-            //     case 6:
-            //     Console.WriteLine("haziran ayındasınız");
-            //     break;
-            //     case 7:
-            //     Console.WriteLine("temmuz ayındasınız");
-            //     break;
-            //     case 8:
-            //     Console.WriteLine("ağustos ayındasınız");
-            //     break;
-            //     case 9:
-            //     Console.WriteLine("Eylül ayındasınız");
-            //     break;
-            //     case 10:
-            //     Console.WriteLine("Ekim ayındasınız");
-            //     break;
-            //     case 11:
-            //     Console.WriteLine("Kasım ayındasınız");
-            //     break;
-            //     case 12:
-            //     Console.WriteLine("Aralık ayındasınız");
-            //     break;
-            //     default:          
-            //     Console.WriteLine("geçersiz ay");
-            //     break;
-            // }
-
-            //Ekrandan girilen sayıya kadar olan tek sayıları ekrana yazdır.
-            // Console.WriteLine("Bir sayı girin");
-            // int sayac = Convert.ToInt32(Console.ReadLine());
-            // for (int i = 0; i <= sayac; i++)
-            // {
-            //     if(i%2==1){
-            //          Console.WriteLine("sayı: "+i);         
-            //     }   
-                                  
-            // }
-            //1 ile 1000 arasındaki tek ve çift sayıların toplamını hesaplayalım ayrı ayrı ekrana yazdıralım
-            // int tek=0;
-            // int cift=0;             
-            // for (int i = 1; i <= 1000; i++)
-            // {
-            //     // if(i == 17) { //atla
-            //     //     continue;
-            //     // }
-            //     // if(i == 30){ //sonlandır
-            //     //     break;
-            //     // }
-            //     if(i %2== 1){
-            //         tek += i;
-            //     }else{
-            //          cift += i;
-            //     }                
-            // }
-            // Console.WriteLine("Tek Sayılar toplamı: {0}",tek);
-            // Console.WriteLine("Çift Sayılar toplamı: {0}",cift);
-
-        //** DÖNGÜLER **//
-        //    Console.WriteLine("bir sayı girin");
-        //    int sayi2= Convert.ToInt32(Console.ReadLine());
-        //    int toplam=0;
-        //    int i=0;
-        //    while (i <= sayi2)
-        //    {
-        //       toplam+=i;
-        //       i++;            
-        //    }
-        //    Console.WriteLine("ortalam: "+ toplam/sayi2);
-        // int [] sayilar = {0,1,2,3,4,5,6,7,8,9,10};
-        // Console.WriteLine(sayilar.Length);
-        // foreach (var item in sayilar)
-        // {
+        }      
             
-        //     if(item != (sayilar.Length-1)){
-        //         Console.Write(item+",");
-        //     }
-        //     else if(item == (sayilar.Length-1))  
-        //     {
-        //          Console.Write(item);
-        //     }
-            
-        // }
-        /****== DİZİLER ==****/
+    }//Program-class-end
 
-        // string[] renkler = {"kırmızı","mavi","yeşil"};
-        // foreach (var item in renkler)
-        // {
-        //     Console.WriteLine(item);
-        // }
-         /**== Array Sınıfı ve Methodlari ==**/
+    class Metotlar{
 
-         //sırasız
-         Console.WriteLine("******sırasız************");
-         int[] sayilar = {10,30,40,50,60,5,3,25};
-         foreach (var item in sayilar)
-         {
-            Console.WriteLine(item);
-         }
-         Console.WriteLine("******sıralı************");
-        //sıralı      
-         Array.Sort(sayilar);  
-         foreach (var item in sayilar)
-         {
-            Console.WriteLine(item);
-         }
-         Console.WriteLine("********Clear************");
-         //Clear
-         Array.Clear(sayilar,2,2);
-          foreach (var item in sayilar)
-         {
-            Console.WriteLine(item);
-         }
-        Console.WriteLine("******Reverse************");
-        //Reverse
-        Array.Reverse(sayilar);
-           foreach (var item in sayilar)
-         {
-            Console.WriteLine(item);
-         }
-         Console.WriteLine("******IndexOf************");
-         //IndexOf
-         var indexOf= Array.IndexOf(sayilar,40);
-          Console.WriteLine(indexOf);
-          Console.WriteLine("******Resize************");
-          //Resize
-          Array.Resize<int>(ref sayilar,5);
-           foreach (var item in sayilar)
-         {
-            Console.WriteLine(item);
-         }
-         
-         }
-      }
+     public void EkranaYazdir(string veri) {
+         Console.WriteLine(veri);
+     }
+     public int Carp(int a, int b){
+       
+       return a*b;
+     }
+     public int ArttirveTopla(ref int deger1,ref int deger2){
+      //orjinal değişken değerini değiştirir/atama yapar
+        deger1+=1;
+        deger2+=1;
+
+      return deger1+deger2;
+     }
     }
+}
