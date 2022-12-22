89d263cbc41b2b07ae61a555a79f1e6e0cf7f62a
ihsan <42092819+ihsanfil@users.noreply.github.com>
2022-12-22 23:26:48 +0300

    string metotlar

--- Program.cs
+++ Program.cs
@@ -6,82 +6,46 @@ namespace degisken
     {
         static void Main(string[] args)
         {
-          //#1 
-          //Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). 
-          //Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
-          //Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
-          //  Console.WriteLine("Kaç Sayı Gireceksiniz");
-          //  int total = Convert.ToInt32(Console.ReadLine());
-          //  List<int> numbers = new List<int>();
-          //  for(int i = 0; i < total; i++){
-          //       Console.WriteLine((i+1)+". sayıyı giriniz");
-          //       int readNumber = Convert.ToInt32(Console.ReadLine());
-          //       numbers.Add(readNumber);
-          //  }
-          //   Console.WriteLine("/****== Girilen Çift Sayılar ==****/");
-          //   foreach(var item in numbers){
-          //     if(item %2 == 0){
-          //       Console.WriteLine(item);
-          //     }
-          //   }
 
+         string degisken = "Dersimiz Csharp2, Hoşgeldiniz";
+         string degisken2 = "Dersimiz Csharp";
+         //#ToUpper - ToLower
+         //Console.WriteLine(degisken.Length);
+         //Console.WriteLine(degisken.ToUpper());
+         //Console.WriteLine(degisken.ToLower());
 
-          //#2
-          //Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). 
-          //Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
-          //Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın. 
+         //#Concat
+         //Console.WriteLine(string.Concat(degisken," Merhaba!"));
+
+         //#Compare - CompareTo
+         //Console.WriteLine(degisken.CompareTo(degisken2)); //0,1,-1
+         //Console.WriteLine(string.Compare(degisken,degisken2,false)); //büyük küçük harf duyarlı olsun
+
+         //#Contains,EndsWith,StartsWith, IndexOf, Insert          
+         //Console.WriteLine(degisken.Contains("Csharp"));
+         //Console.WriteLine(degisken2.EndsWith("Hoşgeldiniz"));
+         //Console.WriteLine(degisken2.StartsWith("Dersimiz"));
+         //Console.WriteLine(degisken.IndexOf("2"));
+         //Console.WriteLine(degisken.Insert(0,"Merhaba "));
+         //Console.WriteLine(degisken.LastIndexOf("rp"));
          
-          //  Console.WriteLine("Kaç sayı gireceksiniz?");
-          //  int totalNum = Convert.ToInt32(Console.ReadLine());
-          //  Console.WriteLine("Baz alınacak sayıyı Giriniz -m");
-          //  int numberM = Convert.ToInt32(Console.ReadLine());
+         //#PadLeft PadRight
+         //Console.WriteLine(degisken+ degisken2.PadLeft(30));
+         //Console.WriteLine(degisken+ degisken2.PadLeft(30,'*'));
+         //Console.WriteLine(degisken.PadRight(50,'-')+ degisken2.PadLeft(30,'*'));
           
-          //  List<int> numbersTwo = new List<int>();
+         //#Remove
+         //Console.WriteLine(degisken.Remove(10,10));
 
-          //  for(int i = 0; i < totalNum; i++){
-          //         Console.WriteLine((i+1)+". sayıyı giriniz");
-          //         int readNumber = Convert.ToInt32(Console.ReadLine());
-          //         numbersTwo.Add(readNumber);
-          //  }
-          
-          //  //m'e eşit yada tam bölünenler
-          //  Console.WriteLine("/****== m'e eşit yada tam bölünenler ==****/");
-          //  foreach(var item in numbersTwo){
-          
-          //   if(item == numberM || numberM % item == 0) {
-          //     Console.WriteLine(item);
-          //   }
-          //  }
-        
-          //#3
-          //Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). 
-          //Sonrasında kullanıcıdan n adet kelime girmesi isteyin. 
-          //Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
-          
-          // Console.WriteLine("Kaç kelime gireceksiniz?");
-          // int totalKey = Convert.ToInt32(Console.ReadLine());
-          // List<string> keyList = new List<string>();
+         //#Replace
+         //Console.WriteLine(degisken.Replace(" ","-"));
 
-          // for(int i= 0; i < totalKey; i++){
-          //    Console.WriteLine((i+1)+". kelimeyi giriniz");
-          //    keyList.Add(Console.ReadLine());
-          // }
-          // Console.WriteLine("/****== Kelimeler ==****/");
-          // keyList.Reverse();
-          // foreach(var item in keyList){
-          //      Console.WriteLine(item);
-          // }
-          
-          //#4
-          //Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. 
-          //Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+         //#Split
+         //Console.WriteLine(degisken.Split(" ")[1]);
 
-          // Console.WriteLine("Bir kelime yazın");
-          // string readstring = Console.ReadLine();
-          // var word = readstring.Split(' ');
-          // var letter = readstring.Replace(" ","");
-          // Console.WriteLine(word.Length+" adet kelime var");
-          // Console.WriteLine(letter.Length+" adet harf var");
+         //#Substring
+         //Console.WriteLine(degisken.Substring(5,10));
+
 
         }//Main      
     }//Program-class-end     
